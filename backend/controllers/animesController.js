c9cb897c005c56b8a8b1eef4b42e7cde7ceee235
Khalid Jameer <41968688+kjameer0@users.noreply.github.com>
2024-07-10 11:15:41 -0400

    fix: changed successful POST request instructions to 201 status code

--- backend/controllers/animesController.js
+++ backend/controllers/animesController.js
@@ -14,7 +14,7 @@ const {
 /* Instructions: Use the following prompts to write the corresponding routes. **Each** route should be able to catch server-side and user input errors(should they apply). Consult the test files to see how the routes and errors should work.*/
 //Write a GET route that retrieves all animes from the database and sends them to the client with a 200 status code
 
-//Write a POST route that takes user provided data from the request body and creates a new anime in the database. The route should respond with a 200 status code and the new anime.
+//Write a POST route that takes user provided data from the request body and creates a new anime in the database. The route should respond with a 201 status code and the new anime.
 //if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
 
 //Write a PUT route that takes user provided data from the request body and updates an existing anime in the database. The route should respond with a 200 and the updated anime. The route should be able to handle a non-existent anime id.
